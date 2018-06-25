6ea97e003a0c50aea84f7191f611142d7a08ec1b
eganichev <evgenii.ganichev@gmail.com>
2018-06-25 09:59:48 +0300

    fix

--- frontend/index.js
+++ frontend/index.js
@@ -2,12 +2,11 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import { Switch, Route, Redirect, Link } from 'react-router-dom'
-import configureStore from './core/store';
 import Main from './container/Main';
 import Game from './components/page/Game';
 import './styles/index.less';
 
-const store = configureStore();
+
 
 class Init extends Component {    
     render() {        
